3276e4f14c48ee9e67fe2f86e6c9db9486359856
lyctea <396136337@qq.com>
2017-01-15 10:38:10 +0800

    配置文件

---
+++ state.js
@@ -0,0 +1,19 @@
+//服务器端数据
+{
+    rooms: [{
+        id, name, owner
+    }]
+}
+
+
+//客户端数据
+{
+        username,
+        current,
+        message: {
+            id: [{
+             content, user, time
+            }],
+            id2: [{}]
+        },
+}
